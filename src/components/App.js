48e2e80bbc9a3605ee5d01c2656244da63482700
nwalberts <nwalberts5@gmail.com>
2021-06-24 12:14:57 -0400

    set main to base app

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import PlaylistCollection from './PlaylistCollection'
-import SongCollection from "./SongCollection"
 
 const App = (props) => {
 
   return (
     <div className="grid-container app">
       <h1 className="title">React Music Player</h1>
-      <div className="grid-x">
-        <PlaylistCollection
-          playlists={props.data.playlists}
-        />
-
-        <SongCollection 
-          songs={props.data.songs}
-        />
-      </div>
     </div>
   );
 }
--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import './main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import data from './constants/data.js';
 
 ReactDOM.render(
-  <App 
-    data={data}
-  />,
+  <App />,
   document.getElementById('app')
 );
